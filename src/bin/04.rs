86e92e69fce2213fe72c4c34e125fb304479804c
Szewek <shevkpl@gmail.com>
2023-12-04 06:56:47 +0100

    Just iterate card matches

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,14 +1,4 @@
 
-struct Card {
-    winning_numbers: Vec<u8>,
-    numbers: Vec<u8>,
-}
-impl Card {
-    fn matches(&self) -> usize {
-        self.numbers.iter().filter(|n| self.winning_numbers.contains(n)).count()
-    }
-}
-
 fn parse_numbers(input: &str) -> Vec<u8> {
     let mut v: Vec<u8> = input.split(' ').filter(|s| !s.is_empty()).map(|s| s.trim().parse().unwrap()).collect();
     v.dedup();
@@ -16,20 +6,18 @@ fn parse_numbers(input: &str) -> Vec<u8> {
     v
 }
 
-fn parse_cards(input: &str) -> impl Iterator<Item = Card> + '_ {
+fn parse_card_matches(input: &str) -> impl Iterator<Item = usize> + '_ {
     input.lines().map(|l| {
         let (_, nums) = l.split_once(':').unwrap();
         let (wn, yn) = nums.split_once('|').unwrap();
-        Card {
-            winning_numbers: parse_numbers(wn),
-            numbers: parse_numbers(yn),
-        }
+        let wn = parse_numbers(wn);
+        let yn = parse_numbers(yn);
+        yn.iter().filter(|n| wn.contains(n)).count()
     })
 }
 
 pub fn part1(input: &str) -> Option<usize> {
-    Some(parse_cards(input).filter_map(|c| {
-        let cnt = c.matches();
+    Some(parse_card_matches(input).filter_map(|cnt| {
         if cnt > 0 {
             Some(1 << (cnt - 1))
         } else {
@@ -39,10 +27,9 @@ pub fn part1(input: &str) -> Option<usize> {
 }
 
 pub fn part2(input: &str) -> Option<u32> {
-    let cards: Vec<_> = parse_cards(input).collect();
-    let mut num_owned = vec![1; cards.len()];
-    for (i, c) in cards.iter().enumerate() {
-        let cnt = c.matches();
+    let card_matches: Vec<_> = parse_card_matches(input).collect();
+    let mut num_owned = vec![1; card_matches.len()];
+    for (i, cnt) in card_matches.iter().enumerate() {
         for j in (i+1)..(num_owned.len().min(i+1+cnt)) {
             num_owned[j] += num_owned[i];
         }
