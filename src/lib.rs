fb9ca128b1d92d81a6c4b3fc1139004176afa711
Szewek <shevkpl@gmail.com>
2023-12-07 20:35:25 +0100

    Fix wrong directory in tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 use std::{fs, fmt, string, time};
 
-#[cfg(test)]
-pub const TEXT_DIR: &str = "examples";
+pub const EXAMPLES: &str = "examples";
+pub const INPUTS: &str = "inputs";
 
-#[cfg(not(test))]
-pub const TEXT_DIR: &str = "inputs";
-
-pub fn read_file_string(name: &str) -> Box<str> {
-    fs::read_to_string(format!("{TEXT_DIR}/{name}.txt"), ).map(string::String::into_boxed_str).unwrap()
+pub fn read_file_string(dir: &str, name: &str) -> Box<str> {
+    fs::read_to_string(format!("{dir}/{name}.txt"), ).map(string::String::into_boxed_str).unwrap()
 }
 
 #[allow(clippy::option_if_let_else)]
@@ -27,14 +24,14 @@ pub fn print_result<P, T: fmt::Display>(part: u32, func: fn(P) -> Option<T>, inp
 macro_rules! solve {
     ($solver1:ident, $solver2:ident) => {
         fn main() {
-            let input = &aoc2023::read_file_string(env!("CARGO_BIN_NAME"));
+            let input = &aoc2023::read_file_string(aoc2023::INPUTS, env!("CARGO_BIN_NAME"));
             aoc2023::print_result(1, $solver1, input);
             aoc2023::print_result(2, $solver2, input);
         }
     };
     ($parser:ident, $solver1:ident, $solver2:ident) => {
         fn main() {
-            let input = &aoc2023::read_file_string(env!("CARGO_BIN_NAME"));
+            let input = &aoc2023::read_file_string(aoc2023::INPUTS, env!("CARGO_BIN_NAME"));
             let pt = std::time::Instant::now();
             let parsed = $parser(input);
             println!("> Parsing time: {:.2?}", pt.elapsed());
@@ -46,11 +43,11 @@ macro_rules! solve {
 #[macro_export]
 macro_rules! assert_ex {
     ($solver:ident, $val:expr) => {
-        let input = aoc2023::read_file_string(env!("CARGO_BIN_NAME"));
+        let input = aoc2023::read_file_string(aoc2023::EXAMPLES, env!("CARGO_BIN_NAME"));
         assert_eq!($solver(&input), Some($val))
     };
     ($parser:ident, $solver:ident, $val:expr) => {
-        let input = aoc2023::read_file_string(env!("CARGO_BIN_NAME"));
+        let input = aoc2023::read_file_string(aoc2023::EXAMPLES, env!("CARGO_BIN_NAME"));
         let parsed = $parser(&input);
         assert_eq!($solver(&parsed), Some($val))
     }
@@ -58,7 +55,7 @@ macro_rules! assert_ex {
 #[macro_export]
 macro_rules! assert_ex_part {
     ($part:expr, $solver:ident, $val:expr) => {
-        let input = aoc2023::read_file_string(&format!("{}_{}", env!("CARGO_BIN_NAME"), $part));
+        let input = aoc2023::read_file_string(aoc2023::EXAMPLES, &format!("{}_{}", env!("CARGO_BIN_NAME"), $part));
         assert_eq!($solver(&input), Some($val))
     };
 }
