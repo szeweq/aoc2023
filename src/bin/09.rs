8cd02220fb37196dbf05db02e5ff5b3e7b75c2de
Szewek <shevkpl@gmail.com>
2023-12-09 07:51:42 +0100

    Store in boxed slices

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,6 +1,8 @@
 use std::vec::IntoIter;
 
-fn calculate_diff_tree(n: Vec<i32>) -> IntoIter<Vec<i32>> {
+type IterRows = IntoIter<Box<[i32]>>;
+
+fn calculate_diff_tree(n: Box<[i32]>) -> IterRows {
     let mut rows = vec![n];
     while let Some(lv) = rows.last().and_then(|v| v.iter().any(|x| *x != 0).then_some(v)) {
         rows.push(lv.windows(2).map(|w| w[1] - w[0]).collect());
@@ -8,8 +10,8 @@ fn calculate_diff_tree(n: Vec<i32>) -> IntoIter<Vec<i32>> {
     rows.into_iter()
 }
 
-fn parse_nums(input: &str) -> impl Iterator<Item = IntoIter<Vec<i32>>> + '_ {
-    input.lines().map(move |l| calculate_diff_tree(l.split_ascii_whitespace().map(|x| x.parse().unwrap()).collect()))
+fn parse_nums(input: &str) -> impl Iterator<Item = IterRows> + '_ {
+    input.lines().map(|l| calculate_diff_tree(l.split_ascii_whitespace().map(|x| x.parse().unwrap()).collect()))
 }
 
 pub fn part1(input: &str) -> Option<i32> {
