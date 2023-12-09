5affeaf2098de501bc839275d497153f2cceb444
Szewek <shevkpl@gmail.com>
2023-12-09 08:02:03 +0100

    Try to write shorter lines

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -2,7 +2,7 @@ use std::vec::IntoIter;
 
 type IterRows = IntoIter<Box<[i32]>>;
 
-fn calculate_diff_tree(n: Box<[i32]>) -> IterRows {
+fn calc_diff_tree(n: Box<[i32]>) -> IterRows {
     let mut rows = vec![n];
     while let Some(lv) = rows.last().and_then(|v| v.iter().any(|x| *x != 0).then_some(v)) {
         rows.push(lv.windows(2).map(|w| w[1] - w[0]).collect());
@@ -11,15 +11,25 @@ fn calculate_diff_tree(n: Box<[i32]>) -> IterRows {
 }
 
 fn parse_nums(input: &str) -> impl Iterator<Item = IterRows> + '_ {
-    input.lines().map(|l| calculate_diff_tree(l.split_ascii_whitespace().map(|x| x.parse().unwrap()).collect()))
+    input.lines().map(|l| calc_diff_tree(
+        l.split_ascii_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect()
+    ))
 }
 
 pub fn part1(input: &str) -> Option<i32> {
-    Some(parse_nums(input).map(|rows| rows.map(|v| v[v.len() - 1]).sum::<i32>()).sum())
+    Some(parse_nums(input)
+        .map(|rows| rows.map(|v| v[v.len() - 1]).sum::<i32>())
+        .sum()
+    )
 }
 
 pub fn part2(input: &str) -> Option<i32> {
-    Some(parse_nums(input).map(|rows| rows.map(|v| v[0]).rfold(0, |acc, x| x - acc)).sum())
+    Some(parse_nums(input)
+        .map(|rows| rows.rfold(0, |acc, v| v[0] - acc))
+        .sum()
+    )
 }
 
 aoc2023::solve!(part1, part2);
