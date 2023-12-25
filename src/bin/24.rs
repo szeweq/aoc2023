b5d53d1a7e1362aaab64bced01f25043900a5ebf
Szewek <shevkpl@gmail.com>
2023-12-25 18:42:54 +0100

    Just a skeleton for day 24, sorry

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,1 +1,27 @@
+/// I will complete this later
 
+pub fn part1(input: &str) -> Option<usize> {
+    None
+}
+
+pub fn part2(input: &str) -> Option<usize> {
+    None
+}
+
+aoc2023::solve!(part1, part2);
+
+#[cfg(test)]
+mod tests {
+    use aoc2023::assert_ex;
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_ex!(part1, 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_ex!(part2, 0);
+    }
+}
